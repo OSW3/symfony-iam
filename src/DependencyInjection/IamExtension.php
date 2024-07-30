15b1e99db92855efe02ac926c151527470294c28
Arnaud BODEL <arnaud.bodel@gmail.com>
2024-07-30 15:05:26 +0200

    -

--- src/DependencyInjection/IamExtension.php
+++ src/DependencyInjection/IamExtension.php
@@ -54,8 +54,24 @@ class IamExtension extends Extension implements PrependExtensionInterface
         // --
 
         $twigConfig = [];
-        $twigConfig['paths'][Path::join(__DIR__, "/../../", "templates")] = "Iam";
+		$this->extendsTwigConfig($twigConfig, Path::join(__DIR__, "/../../", "templates"), "Iam");
 
         $container->prependExtensionConfig('twig', $twigConfig);
     }
+
+	/**
+	 * Add a path to extends twig sources
+	 *
+	 * @param array $twigConfig
+	 * @param string $directory
+	 * @param string $alias
+	 * @return void
+	 */
+	private function extendsTwigConfig(array &$twigConfig, string $directory, string $alias) 
+	{
+		if (is_dir($directory))
+		{
+			$twigConfig['paths'][$directory] = $alias;
+		}
+	}
 }
